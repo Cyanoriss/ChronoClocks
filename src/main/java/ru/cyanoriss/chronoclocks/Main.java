52cab7c75eb22816a33d2edf53cda247daba70c2
BuildTools <unconfigured@null.spigotmc.org>
2024-02-26 18:52:39 +0300

    Тестовая команда для работы со шрифтами

--- src/main/java/ru/cyanoriss/chronoclocks/Main.java
+++ src/main/java/ru/cyanoriss/chronoclocks/Main.java
@@ -2,10 +2,28 @@ package ru.cyanoriss.chronoclocks;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
 public final class Main extends JavaPlugin {
+    private static Main instance;
+
+    public static Main getInstance() {
+        return instance;
+    }
 
     @Override
     public void onEnable() {
-        getLogger().info("Тест");
+        instance = this;
+        getCommand("test").setExecutor(new TestCommand());
+        LocalDateTime rawTime = LocalDateTime.now(ZoneId.of("UTC+4"));
+
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("HH:mm:ss");
+
+        String formattedTime = rawTime.format(format);
+
+        getLogger().info(formattedTime);
+
     }
 }
